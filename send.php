e0a1e49e30579186a8ac51e61247a87135262f59
Sergey-ET <sergey_troshkin@hotmail.com>
2021-11-17 21:56:13 +0300

    Update email messages from send.php

--- send.php
+++ send.php
@@ -4,21 +4,45 @@ require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-// Переменные, которые отправляет пользователь
-$theme = $_POST['theme'];
-$message = $_POST['message'];
-$email = $_POST['email'];
-$comment = $_POST['comment'];
-
-// Формирование самого письма
-$title = 'Новая заявка с сайта Universal Magazine';
-$body = "
-<h2>Новая Заявка</h2>
-<b>Тема:</b> $theme<br>
-<b>Email:</b> $email<br><br>
-<b>Сообщение:</b><br>$message<br><br>
-<b>Комментарий:</b><br>$comment
-";
+if (isset($_POST['email-send'])) {
+    $email = $_POST['email'];
+    $title = 'Новая подписка с сайта Universal Magazine';
+    $body = "
+  <h2>Новая подписка</h2>
+  <b>Email:</b> $email<br>
+  ";
+} elseif (isset($_POST['comment-send'])) {
+    $comment = $_POST['comment'];
+    $title = 'Новый комментарий с сайта Universal Magazine';
+    $body = "
+  <h2>Новый комментарий</h2>
+  <b>Комментарий:</b></br>$comment
+  ";
+} elseif (isset($_POST['modal-send'])) {
+    $theme = $_POST['theme'];
+    $message = $_POST['message'];
+    $email = $_POST['email'];
+    $title = 'Новая заявка с сайта Universal Magazine';
+    $body = "
+  <h2>Новая Заявка</h2>
+  <b>Тема:</b> $theme<br>
+  <b>Email:</b> $email<br><br>
+  <b>Сообщение:</b><br>$message<br>
+  ";
+} else {
+    $email = $_POST['email'];
+    $comment = $_POST['comment'];
+    $theme = $_POST['theme'];
+    $message = $_POST['message'];
+    $title = 'Ошибочное сообщение с сайта Universal Magazine';
+    $body = "
+  <h2>Ошибочное сообщение</h2>
+  <b>Тема:</b> $theme<br>
+  <b>Email:</b> $email<br><br>
+  <b>Сообщение:</b><br>$message<br><br>
+  <b>Комментарий:</b><br>$comment
+  ";
+}
 
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
